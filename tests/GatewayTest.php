8bcc4807c59c2f76596ad1ded0e7b5ef80892d27
Christian Freear <christianfreear@gmail.com>
2014-11-30 15:03:35 +0000

    Purchase tests

--- tests/GatewayTest.php
+++ tests/GatewayTest.php
@@ -10,6 +10,24 @@ class GatewayTest extends GatewayTestCase
     {
         parent::setUp();
 
-        $this->gateway = new Gateway($this->getHttpClient(), $this->getHttpRequest());
+        $this->gateway = new RemoteGateway($this->getHttpClient(), $this->getHttpRequest());
+    }
+
+    public function testPurchase()
+    {
+        $request = $this->gateway->purchase(array('amount' => '10.00'));
+
+        $this->assertInstanceOf('Omnipay\Relex\Message\AuthRequest', $request);
+        $this->assertSame('10.00', $request->getAmount());
+    }
+
+    public function testPurchase3dSecure()
+    {
+        $this->gateway->set3dSecure(1);
+
+        $request = $this->gateway->purchase(array('amount' => '10.00'));
+
+        $this->assertInstanceOf('Omnipay\Relex\Message\EnrolmentRequest', $request);
+        $this->assertSame('10.00', $request->getAmount());
     }
 }
